ba367b69066dd7fede409899c23b2866d1c38e41
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-08-01 17:39:44 -0300

    criacao do preview das imagens selecionadas pelo usuario

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,13 +1,19 @@
-import React, {useState} from 'react';
+import React, {useState, useMemo} from 'react';
 
 import camera from '../../assets/camera.svg';
 
 import './styles.css'
 
 export default function New() {
+    const [thumbnail, setThumbnail] = useState(null);
     const [company, setCompany] = useState('');
     const [techs, setTechs] = useState('');
     const [price, setPrice] = useState('');
+
+    // sempre que a thumbnail for atualizada cria-se um preview da imagem dela
+    const preview = useMemo(() => {
+        return thumbnail ? URL.createObjectURL(thumbnail) : null;
+    }, [thumbnail]) // array de quando ele deve executar
 
     function handleSubmit() {
 
@@ -15,8 +21,8 @@ export default function New() {
 
     return(
         <form onSubmit={handleSubmit}>
-            <label id="thumbnail">
-                <input type="file"/> 
+            <label id="thumbnail" style={{ backgroundImage: `url(${preview})` }} >
+                <input type="file" onChange={event => setThumbnail(event.target.files[0])} /> 
                 <img id="cam-icon" src={camera} alt="Select img" />
             </label>
 
@@ -51,3 +57,6 @@ export default function New() {
 
 // OBSERVAÇÕES
 // através do <input type="file"/>  que conseguiremos o upload
+// a variavel mutiple permite que o usuario selecione mais de um arquivo
+// sempre que alteramos um estado do react o programa executa a função novamente tudo do zero
+// createObjectURL cria uma URL para uma variavel temporaria
