96842b0c2efe81c8a8ba84d7cc256dbcb8509053
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-08-01 16:24:15 -0300

    linkando rotas utilizando o Link do react router dom

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../../service/api';
+import './styles.css';
 
 export default function Dashboard() {
     const [spots, setSpots] = useState([]); // array vazio pq receberei um array dentro dessa variavel 
@@ -22,15 +24,21 @@ export default function Dashboard() {
             <ul className="spot-list">
                 {spots.map(spot => (
                     <li key={spot._id}>
-                        <header />
+                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} /> 
                         <strong>{spot.company}</strong>
-                        <span>{spot.price}</span>
+                        <span>{spot.price ? `R${spot.price}/dia` : 'Gratuito'}</span>
                     </li>
                 ))}
             </ul>
+
+            <Link to="/new" >
+                Cadastrar novo spot
+            </Link>
         </>
     )
 }
 
 // OBSERVAÇÕES 
 // useEffect recebe dois parametros: uma função e um array de dependencias (passa o filtro da listagem e toda vez que esse array for alterado a função será executada novamente, se a gente passa esse array vazio a função só será executada uma única vez)
+// quando usamos duas chaves seguidas significa que a primeira chave indica que o usuario esta colocando um codigo javascript e a segunda que é um objeto
+// Link: quando o usuario clica vai para outra rota de maneira mais fácil que o history.
