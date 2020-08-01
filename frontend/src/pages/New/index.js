d90683be64a28594e29f997be35c02fd90da80f2
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-08-01 18:19:02 -0300

    finalizacao do frontend web

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,10 +1,11 @@
 import React, {useState, useMemo} from 'react';
+import api from '../../service/api';
 
 import camera from '../../assets/camera.svg';
 
 import './styles.css'
 
-export default function New() {
+export default function New({ history }) {
     const [thumbnail, setThumbnail] = useState(null);
     const [company, setCompany] = useState('');
     const [techs, setTechs] = useState('');
@@ -15,13 +16,31 @@ export default function New() {
         return thumbnail ? URL.createObjectURL(thumbnail) : null;
     }, [thumbnail]) // array de quando ele deve executar
 
-    function handleSubmit() {
+    async function handleSubmit(event) {
+        event.preventDefault(); // faz com que a aplicação nao refirecione para uma pagina padrão
 
+        const data = new FormData();
+        const user_id = localStorage.getItem('user');
+
+        data.append('thumbnail', thumbnail);
+        data.append('company', company);
+        data.append('techs', techs);
+        data.append('price', price);
+
+        await api.post('/spots', data, {
+            headers: { user_id }
+        }) // informando a url, o conteudo que será salvo e quem criou essa spot
+
+        history.push('/dashboard');
     }
 
     return(
         <form onSubmit={handleSubmit}>
-            <label id="thumbnail" style={{ backgroundImage: `url(${preview})` }} >
+            <label 
+                id="thumbnail" 
+                style={{ backgroundImage: `url(${preview})` }} 
+                className={thumbnail ? 'has-thumbnail' : ''}
+            >
                 <input type="file" onChange={event => setThumbnail(event.target.files[0])} /> 
                 <img id="cam-icon" src={camera} alt="Select img" />
             </label>
